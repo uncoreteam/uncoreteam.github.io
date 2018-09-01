fc8040d3eb1901bf7ebd1d1d6865630dc46bb98c
primakashi <cc.primadani@gmail.com>
2018-09-01 17:33:12 +0700

    email submit

--- js/plugin/pollyfill.js
+++ js/plugin/pollyfill.js
@@ -24,8 +24,8 @@
         points = [];
         for (var x = 0; x < width; x = x + width / 20) {
             for (var y = 0; y < height; y = y + height / 20) {
-                var px = x + Math.random() * width / 20;
-                var py = y + Math.random() * height / 20;
+                var px = x + Math.random() * width / 10;
+                var py = y + Math.random() * height / 10;
                 var p = { x: px, originX: px, y: py, originY: py };
                 points.push(p);
             }
